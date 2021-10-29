dda5440c82076244f7e9359a0dac9b932aeb5a0e
WAvery2000 <43053338+WAvery2000@users.noreply.github.com>
2021-10-29 16:33:18 -0500

    Added FAT drivers and tests

--- src/Macropad.c
+++ src/Macropad.c
@@ -1,10 +1,27 @@
+/**
+ * Filename: Macropad.c
+ * Authors: William Avery (add names here)
+ * Last Modified: 10/29/2021
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include "../inc/CortexM.h"
 #include "../inc/PLL.h"
+#include "../inc/ff.h"
+#include "../inc/diskio.h"
+#include "../inc/ST7735_SDC.h"
 #include "SwitchMatrix.h"
 #include "RotarySwitch.h"
 
+/**
+ * Choose the test loop to run
+ * 0 - Main Control Loop
+ * 1 - SD Card Test
+ */
+#define __MAIN__ 1
+
+#if __MAIN__ == 0
 int main(void)
 {
   PLL_Init(Bus80MHz);
@@ -18,3 +35,111 @@ int main(void)
     SwitchMatrix_CycleColumnOutput();
   }
 }
+
+#elif __MAIN__ == 1
+static FATFS g_sFatFs;
+FIL Handle,Handle2;
+FRESULT MountFresult;
+FRESULT Fresult;
+unsigned char buffer[512];
+const char inFilename[] = "test.txt";   // 8 characters or fewer
+const char outFilename[] = "out.txt";   // 8 characters or fewer
+
+int main(void) {
+  UINT successfulreads, successfulwrites;
+  uint8_t c, x, y;
+  
+  PLL_Init(Bus80MHz);
+  DisableInterrupts();
+  ST7735_InitR(INITR_REDTAB);
+  ST7735_FillScreen(0); // set screen to black
+  EnableInterrupts();
+  
+  MountFresult = f_mount(&g_sFatFs, "", 0); // mount the filesystem
+  if(MountFresult){
+    ST7735_DrawString(0, 0, "f_mount error", ST7735_Color565(0, 0, 255));
+    while(1){};
+  }
+  Fresult = f_open(&Handle, inFilename, FA_READ); // open test.txt
+  if(Fresult == FR_OK){
+    ST7735_DrawString(0, 0, "Opened ", ST7735_Color565(0, 255, 0));
+    ST7735_DrawString(7, 0, (char *)inFilename, ST7735_Color565(0, 255, 0));
+    // get a character in 'c' and the number of successful reads in 'successfulreads'
+    Fresult = f_read(&Handle, &c, 1, &successfulreads);
+    x = 0;                              // start in the first column
+    y = 10;                             // start in the second row
+    while((Fresult == FR_OK) && (successfulreads == 1) && (y <= 130)){
+      if(c == '\n'){
+        x = 0;                          // go to the first column (this seems implied)
+        y = y + 10;                     // go to the next row
+      } else if(c == '\r'){
+        x = 0;                          // go to the first column
+      } else{                           // the character is printable, so print it
+        ST7735_DrawChar(x, y, c, ST7735_Color565(255, 255, 255), 0, 1);
+        x = x + 6;                      // go to the next column
+        if(x > 122){                    // reached the right edge of the screen
+          x = 0;                        // go to the first column
+          y = y + 10;                   // go to the next row
+        }
+      }
+      // get the next character in 'c'
+      Fresult = f_read(&Handle, &c, 1, &successfulreads);
+    }
+    // close the file
+    Fresult = f_close(&Handle);
+  } else{
+    // print the error code
+    ST7735_DrawString(0, 0, "Error          (  )", ST7735_Color565(255, 0, 0));
+    ST7735_DrawString(6, 0, (char *)inFilename, ST7735_Color565(255, 0, 0));
+    ST7735_SetCursor(16, 0);
+    ST7735_SetTextColor(ST7735_Color565(255, 0, 0));
+    ST7735_OutUDec((uint32_t)Fresult);
+  }
+
+  // open out.txt
+  // Options:
+  // FA_CREATE_NEW    - Creates a new file, only if it does not already exist.  If file already exists, the function fails.
+  // FA_CREATE_ALWAYS - Creates a new file, always.  If file already exists, it is over-written.
+  // FA_OPEN_ALWAYS   - Opens a file, always.  If file does not exist, the function creates a file.
+  // FA_OPEN_EXISTING - Opens a file, only if it exists.  If the file does not exist, the function fails.
+  Fresult = f_open(&Handle, outFilename, FA_WRITE|FA_OPEN_ALWAYS);
+  if(Fresult == FR_OK){
+    ST7735_DrawString(0, 14, "Opened ", ST7735_Color565(0, 255, 0));
+    ST7735_DrawString(7, 14, (char *)outFilename, ST7735_Color565(0, 255, 0));
+    // jump to the end of the file
+    Fresult = f_lseek(&Handle, Handle.fsize);
+    // write a message and get the number of successful writes in 'successfulwrites'
+    Fresult = f_write(&Handle, "Another successful write.\r\n", 27, &successfulwrites);
+    if(Fresult == FR_OK){
+      // print the number of successful writes
+      // expect: third parameter of f_write()
+      ST7735_DrawString(0, 15, "Writes:    @", ST7735_Color565(0, 255, 0));
+      ST7735_SetCursor(8, 15);
+      ST7735_SetTextColor(ST7735_Color565(255, 255, 255));
+      ST7735_OutUDec((uint32_t)successfulwrites);
+      ST7735_SetCursor(13, 15);
+      // print the byte offset from the start of the file where the writes started
+      // expect: (third parameter of f_write())*(number of times this program has been run before)
+      ST7735_OutUDec((uint32_t)(Handle.fptr - successfulwrites));
+    } else{
+      // print the error code
+      ST7735_DrawString(0, 15, "f_write() error (  )", ST7735_Color565(255, 0, 0));
+      ST7735_SetCursor(17, 15);
+      ST7735_SetTextColor(ST7735_Color565(255, 0, 0));
+      ST7735_OutUDec((uint32_t)Fresult);
+    }
+    // close the file
+    Fresult = f_close(&Handle);
+  } else{
+    // print the error code
+    ST7735_DrawString(0, 14, "Error          (  )", ST7735_Color565(255, 0, 0));
+    ST7735_DrawString(6, 14, (char *)outFilename, ST7735_Color565(255, 0, 0));
+    ST7735_SetCursor(16, 14);
+    ST7735_SetTextColor(ST7735_Color565(255, 0, 0));
+    ST7735_OutUDec((uint32_t)Fresult);
+  }
+  while(1){};
+}
+#endif
+
+
